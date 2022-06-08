6835825743ee75ced38b82a50eb410e72a56338b
alexandr-stanchik <lex023ott@mail.ru>
2022-06-08 21:52:49 +0300

    first commit

---
+++ lesson_14_1_hw.py
@@ -0,0 +1,29 @@
+class Dog:
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+
+    def jump(self):
+        print(f'{self.name} is jumping')
+
+    def run(self):
+        print(f'{self.name} is running')
+
+    def bark(self):
+        print(f'{self.name} is barking')
+
+    def change_name(self, new_name):
+        self.name = new_name
+
+
+dog = Dog('Max', 5, 0.5, 15)
+dog.jump()
+dog.run()
+dog.jump()
+dog.__dict__
+
+print(f"Dog's name is {dog.name.capitalize()}")
+dog.change_name(input(f'Type new name for dog: ').capitalize())
+print(f"Dog's new name is {dog.name.capitalize()}")
